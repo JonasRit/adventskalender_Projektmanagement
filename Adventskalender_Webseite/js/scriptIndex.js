eaa69e744cad7cf8a362f1792934fbc42b52cd62
Jonas Ritter <Jonas@Ritter-schutterwald.de>
2026-01-09 12:27:35 +0100

    kommentar wieder weg gemacht

--- Adventskalender_Webseite/js/scriptIndex.js
+++ Adventskalender_Webseite/js/scriptIndex.js
@@ -345,8 +345,6 @@ https://www.welt.de/vermischtes/article2940187/Amoklauf-Weihnachtsmann-macht-16-
     const fensterTitel = document.getElementById('fensterTitel');
     const fensterText = document.getElementById('fensterText');
 
-    //hier ist ein test
-
     //hier wird definiert was passiert wenn irgendein Türchen
     kalender.addEventListener('click', function(e) {
         const tuerchen = e.target.closest('.tuerchen');
